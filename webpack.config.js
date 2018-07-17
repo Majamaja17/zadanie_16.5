d7f1f465031eed38020528f6a335299188741fcc
Maja Jelenik <maja.jelenik@vp.pl>
2018-07-17 23:52:01 +0200

    Change webpack.config file

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,7 @@ module.exports = {
             path: path.resolve(__dirname, 'build'),
             filename: 'app.' + environment + '.bundle.js'
         },
+        
     module: {
         rules: [
             {
